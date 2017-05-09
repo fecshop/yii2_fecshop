ac138560cd663aee73c9a405f15ee4bc3848a8ce
root <root@iZ942k2d5ezZ.(none)>
2017-05-09 10:51:27 +0800

    更改成BSD开源协议

--- app/appfront/theme/base/front/catalog/category/index/filter/attr.php
+++ app/appfront/theme/base/front/catalog/category/index/filter/attr.php
@@ -16,21 +16,29 @@ if(isset($parentThis['filters']) && !empty($parentThis['filters']) && is_array($
 	foreach($parentThis['filters']  as $attr => $filter){
 		$attrUrlStr = Yii::$service->url->category->attrValConvertUrlStr($attr);
 		if(is_array($filter) && !empty($filter)){
-			echo '<div class="filter_attr">';
-			echo '<div class="filter_attr_title">'.Yii::$service->page->translate->__($attr).'</div>';
-			echo '<div class="filter_attr_info">';
+			$str = '';
+			$str .= '<div class="filter_attr">';
+			$str .= '<div class="filter_attr_title">'.Yii::$service->page->translate->__($attr).'</div>';
+			$str .= '<div class="filter_attr_info">';
+			$i = 0;
 			foreach($filter as $item){
 				$val = $item['_id'];
 				$count = $item['count'];
 				if($val){
+					$i++;
+					if($i == 1){
+						echo $str;
+					}
 					$urlInfo = Yii::$service->url->category->getFilterChooseAttrUrl($attrUrlStr,$val,'p');
 					$url = $urlInfo['url'];
 					$selected = $urlInfo['selected'] ? 'class="checked"' : '';
 					echo '<a '.$selected.' href="'.$url.'">'.Yii::$service->page->translate->__($val).'('.$count.')</a><br/>';
 				}
 			}
-			echo '</div>';
-			echo '</div>';
+			if($i >= 1){ 
+				echo '</div>';
+				echo '</div>';
+			}
 		}
 	}
 }
