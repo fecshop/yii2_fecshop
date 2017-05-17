390081dfab061ffd7d49af2a92bb40dbc21d87e4
root <root@iZ942k2d5ezZ.(none)>
2017-05-17 10:21:44 +0800

    apphtml5 theme : account index

--- app/apphtml5/theme/base/html5/customer/leftmenu.php
+++ app/apphtml5/theme/base/html5/customer/leftmenu.php
@@ -17,5 +17,5 @@
 </div>
 
 <div class="account_footer">
-	<a external  href="<?= Yii::$service->url->getUrl("customer/account/logout");?> " class="ui-link">Logout</a>
+	<a   external  href="<?= Yii::$service->url->getUrl("customer/account/logout");?> " class="button button-fill button-bbb">Logout</a>
 </div>
