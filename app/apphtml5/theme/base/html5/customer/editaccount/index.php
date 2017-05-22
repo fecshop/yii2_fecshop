c1391cac8385522fe31b3cf35d399511c4c91dd6
root <root@iZ942k2d5ezZ.(none)>
2017-05-22 12:10:19 +0800

    apphtml5：edit account

--- app/apphtml5/theme/base/html5/customer/editaccount/index.php
+++ app/apphtml5/theme/base/html5/customer/editaccount/index.php
@@ -5,115 +5,123 @@
 		</a>
 		<h1 class='title'><?= Yii::$service->page->translate->__('Edit Account'); ?></h1>
 	</div>
-
-
-
-
-
-
-<div class="main container two-columns-left">
+</div>
 <?= Yii::$service->page->widget->render('flashmessage'); ?>
 
-	<div class="col-main account_center">
-		<div class="std">
-			<div style="margin:4px 0 0">
-				<div class="page-title">
-					<h2><?= Yii::$service->page->translate->__('Edit Account Information');?></h2>
+
+<div class="list-block customer-login  customer-register">
+	<form method="post" id="form-validate" autocomplete="off" action="<?=  $actionUrl ?>">
+		<?= \fec\helpers\CRequest::getCsrfInputHtml();  ?>
+		<ul>
+			<li>
+				<div class="item-content">
+					<div class="item-media">
+						<i class="icon icon-form-name"></i>
+					</div>
+					<div class="item-inner">
+						<div class="item-input">
+							<input type="text" placeholder="<?= Yii::$service->page->translate->__('Email Address');?>"  style="color:#ccc;" readonly="true" id="customer_email" name="editForm[email]" value="<?= $email ?>" title="Email"  class="input-text required-entry" />
+						</div>
+					</div>
 				</div>
-				<form method="post" id="form-validate" autocomplete="off" action="<?=  $actionUrl ?>">
-					<?= \fec\helpers\CRequest::getCsrfInputHtml();  ?>
-					<div class="">
-						<ul class="">
-							<li>
-								<label for="email" class="required"><?= Yii::$service->page->translate->__('Email Address');?></label>
-								<div class="input-box">
-									
-									<input style="color:#ccc;" readonly="true" id="customer_email" name="editForm[email]" value="<?= $email ?>" title="Email" maxlength="255" class="input-text required-entry" type="text">
-									
-								</div>
-							</li>
-							<li class="">
+			</li>
+			<li>
+				<div class="item-content">
+					<div class="item-media">
+						<i class="icon icon-form-name"></i>
+					</div>
+					<div class="item-inner">
+						<div class="item-input">
+							<input  placeholder="First name" id="firstname" name="editForm[firstname]" value="<?= $firstname ?>" title="First Name"  class="input-text required-entry" type="text"  />
+							<div class="validation-advice" id="required_current_firstname" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="item-content">
+					<div class="item-media">
+						<i class="icon icon-form-name"></i>
+					</div>
+					<div class="item-inner">
+						<div class="item-input">
+							<input  type="text" placeholder="Last name" id="lastname" name="editForm[lastname]" value="<?= $lastname ?>" title="Last Name" maxlength="255" class="input-text required-entry" />
+							<div class="validation-advice" id="required_current_lastname" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
+						</div>
+					</div>
+				</div>
+			</li>
+			
+			<li class="control">
+				<div class="change_password_label item-content">
+					<input name="editForm[change_password]" id="change_password" value="1" onclick="setPasswordForm(this.checked)" title="Change Password" class="checkbox" type="checkbox">
+					<label for="change_password"><?= Yii::$service->page->translate->__('Change Password');?></label>
+				</div>
+			</li>
+			
+			<li class="fieldset_pass" style="display:none">
+				<div class="item-content">
+					<div class="item-media">
+						<i class="icon icon-form-name"></i>
+					</div>
+					<div class="item-inner">
+						<div class="item-input">
+							<input placeholder="current password" title="Current Password" class="input-text required-entry" name="editForm[current_password]" id="current_password" type="password" />
+							<div class="validation-advice" id="required_current_password" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
 								
-									<div class="field name-firstname">
-										<label for="firstname" class="required"><?= Yii::$service->page->translate->__('First Name');?></label>
-										<div class="input-box">
-											<input id="firstname" name="editForm[firstname]" value="<?= $firstname ?>" title="First Name" maxlength="255" class="input-text required-entry" type="text">
-											<div class="validation-advice" id="required_current_firstname" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
-										</div>
-									</div>
-							</li>
-							<li>
-								<div class="field name-lastname">
-										<label for="lastname" class="required"><?= Yii::$service->page->translate->__('Last Name');?></label>
-										<div class="input-box">
-											<input id="lastname" name="editForm[lastname]" value="<?= $lastname ?>" title="Last Name" maxlength="255" class="input-text required-entry" type="text">
-											<div class="validation-advice" id="required_current_lastname" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
-										</div>
-									</div>
-							</li>
-							<li class="control">
-								<input name="editForm[change_password]" id="change_password" value="1" onclick="setPasswordForm(this.checked)" title="Change Password" class="checkbox" type="checkbox">
-								<label style="display:inline;" for="change_password"><?= Yii::$service->page->translate->__('Change Password');?></label>
-							</li>
-						</ul>
+						</div>
 					</div>
-				
-					<div class="" id="fieldset_pass" style="display:none;">
-						
-						<ul class="form-list">
-							<li>
-								<label style="font-weight:100;" for="current_password" class="required"><?= Yii::$service->page->translate->__('Current Password');?></label>
-								<div class="input-box">
-									<input title="Current Password" class="input-text required-entry" name="editForm[current_password]" id="current_password" type="password">
-									<div class="validation-advice" id="required_current_password" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
-								</div>
-							</li>
-							<li class="fields">
-								<div class="field">
-									<label style="font-weight:100;" for="password" class="required"><?= Yii::$service->page->translate->__('New Password');?></label>
-									<div class="input-box">
-										<input title="New Password" class="input-text validate-password required-entry" name="editForm[password]" id="password" type="password">
-										<div class="validation-advice" id="required_new_password" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
-									</div>
-								</div>
-								<div class="field">
-									<label style="font-weight:100;" for="confirmation" class="required"><em>*</em><?= Yii::$service->page->translate->__('Confirm New Password');?></label>
-									<div class="input-box">
-										<input title="Confirm New Password" class="input-text validate-cpassword required-entry" name="editForm[confirmation]" id="confirmation" type="password">
-										<div class="validation-advice" id="required_confirm_password" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
-									</div>
-								</div>
-								<div class="clear"></div>
-							</li>
-						</ul>
+				</div>
+			</li>
+			
+			<li class="fieldset_pass" style="display:none">
+				<div class="item-content">
+					<div class="item-media">
+						<i class="icon icon-form-name"></i>
 					</div>
-					<div class="buttons-set">
-						<button type="submit" title="Save" class="button" onclick="return check_edit()"><span><span><?= Yii::$service->page->translate->__('Submit');?></span></span></button>
+					<div class="item-inner">
+						<div class="item-input">
+							<input placeholder="New password" title="New Password" class="input-text validate-password required-entry" name="editForm[password]" id="password" type="password" />
+							<div class="validation-advice" id="required_new_password" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>	
+						</div>
 					</div>
-				</form>
-			</div>
+				</div>
+			</li>
+			
+			<li class="fieldset_pass" style="display:none">
+				<div class="item-content">
+					<div class="item-media">
+						<i class="icon icon-form-name"></i>
+					</div>
+					<div class="item-inner">
+						<div class="item-input">
+							<input placeholder="Confirm New Password"  title="Confirm New Password" class="input-text validate-cpassword required-entry" name="editForm[confirmation]" id="confirmation" type="password"  />
+							<div class="validation-advice" id="required_confirm_password" style="display:none;"><?= Yii::$service->page->translate->__('This is a required field.');?></div>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div class="clear"></div>
+		<div class="buttons-set">
+			<p>
+				<a external href="#"  id="js_editBtn" class="button button-fill">
+					<?= Yii::$service->page->translate->__('Edit Account'); ?>
+				</a>
+			</p>
 		</div>
-	</div>
-	
-	<div class="col-left ">
-		<?php
-			$leftMenu = [
-				'class' => 'fecshop\app\apphtml5\modules\Customer\block\LeftMenu',
-				'view'	=> 'customer/leftmenu.php'
-			];
-		?>
-		<?= Yii::$service->page->widget->render($leftMenu,$this); ?>
-	</div>
-	<div class="clear"></div>
+		
+	</form>
 </div>
 
 <script>
 <?php $this->beginBlock('customer_account_info_update') ?> 
+	
 	function setPasswordForm(arg){
-        if(arg){
-            $('#fieldset_pass').show();
+		if(arg){
+            $('.fieldset_pass').show();
         }else{
-            $('#fieldset_pass').hide();
+            $('.fieldset_pass').hide();
         }
     }
     function check_edit(){
@@ -153,7 +161,7 @@
             $confirmation = $('#confirmation').val();
             if($current_password == ''){
                $('#current_password').addClass('validation-failed');
-               $('#required_current_password').show();
+               $('.required_current_password').show();
                $check_current_password = false;
             }else{
                $('#current_password').removeClass('validation-failed');
@@ -223,6 +231,13 @@
         if(myReg.test(str)) return true; 
         return false; 
     } 
+	$(document).ready(function(){
+		$("#js_editBtn").click(function(){
+			if(check_edit()){
+				$("#form-validate").submit();
+			}
+		});
+	});
 <?php $this->endBlock(); ?>  
 </script>  
 <?php $this->registerJs($this->blocks['customer_account_info_update'],\yii\web\View::POS_END);//将编写的js代码注册到页面底部 ?>
