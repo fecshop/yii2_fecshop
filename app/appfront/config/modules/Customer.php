865fabd8a55ee9ff97fdc63c243bca24adf7ee5d
Terry <2358269014@qq.com>
2017-07-06 09:54:32 +0800

    update

--- app/appfront/config/modules/Customer.php
+++ app/appfront/config/modules/Customer.php
@@ -34,14 +34,14 @@ return [
                 'forgotCaptcha' => true,
 
             ],
-
+            // 在账户中心左侧栏显示的菜单。
             'leftMenu'  => [
-                'Account Dashboard' => 'customer/account',
-                'Account Information' => 'customer/editaccount',
-                'Address Book' => 'customer/address',
-                'My Orders' => 'customer/order',
-                'My Product Reviews' => 'customer/productreview',
-                'My Favorite' => 'customer/productfavorite',
+                'Account Dashboard'     => 'customer/account',
+                'Account Information'   => 'customer/editaccount',
+                'Address Book'          => 'customer/address',
+                'My Orders'             => 'customer/order',
+                'My Product Reviews'    => 'customer/productreview',
+                'My Favorite'           => 'customer/productfavorite',
 
             ],
 
