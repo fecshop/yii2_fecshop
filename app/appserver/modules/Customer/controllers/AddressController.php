a17f4c823ecab0fa71cd11fc20837dfd05d77f22
Terry <2358269014@qq.com>
2017-09-22 18:27:19 +0800

    customer account address

--- app/appserver/modules/Customer/controllers/AddressController.php
+++ app/appserver/modules/Customer/controllers/AddressController.php
@@ -32,30 +32,62 @@ class AddressController extends AppserverTokenController
     }
     
     
-    public function coll()
-    {
-        $identity = Yii::$app->user->identity;
-        $customer_id = $identity['id'];
-        $filter = [
-                'numPerPage'    => 100,
-                'pageNum'        => 1,
-                'orderBy'    => ['updated_at' => SORT_DESC],
-                'where'            => [
-                    ['customer_id' => $customer_id],
-                ],
-            'asArray' => true,
-          ];
-        $coll = Yii::$service->customer->address->coll($filter);
-        $arr = [];
-        if (isset($coll['coll']) && !empty($coll['coll'])) {
-            foreach($coll['coll'] as $one){
-                $one['stateName'] = Yii::$service->helper->country->getStateByContryCode($one['country'],$one['state']);
-                $one['countryName'] = Yii::$service->helper->country->getCountryNameByKey($one['country']); 
-                $arr[] = $one;
+    public function actionEdit(){
+        $address = [];
+        $country = '';
+        $address_id = Yii::$app->request->get('address_id');
+            if($address_id){
+            $addressModel = Yii::$service->customer->address->getByPrimaryKey($address_id);
+            $identity = Yii::$app->user->identity;
+            $customer_id = $identity['id'];
+            
+            if ($addressModel['address_id']) {
+                // 该id必须是当前用户的
+                if ($customer_id == $addressModel['customer_id']) {
+                    foreach ($addressModel as $k=>$v) {
+                        $address[$k] = $v;
+                    }
+                }
             }
+            $country = isset($address['country']) ? $address['country'] : '';
+            
+        }else{
+            
         }
-        return $arr;
+        if(!$country){
+            $country = Yii::$service->helper->country->getDefaultCountry();
+        }
+        $countryArr = Yii::$service->helper->country->getAllCountryArray();
+        $address['countryArr'] = $countryArr;
+        
+        
+        
+        $state = isset($address['state']) ? $address['state'] : '';
+        $stateArr = Yii::$service->helper->country->getStateByContryCode($country);
+        $stateIsSelect = 0;
+        if(!empty($stateArr)){
+            $stateIsSelect = 1;
+        }
+        $address['stateArr'] = $stateArr;
+        $address['stateIsSelect'] = $stateIsSelect;
+        
+        // if (!$stateHtml) {
+        //    $stateHtml = '<input id="state" name="address[state]" value="'.$state.'" title="State" class="input-text" style="" type="text">';
+        // } else {
+        //    $stateHtml = '<select id="address:state" class="address_state validate-select" title="State" name="address[state]">
+		//					<option value="">Please select region, state or province</option>'
+        //                .$stateHtml.'</select>';
+        // }
+        // $this->_address['stateHtml'] = $stateHtml;
+
+        return [
+            'code' => 200,
+            'address' => $address,
+        ];
+        
     }
+    
+    
     
     public function actionRemove(){
         $address_id = Yii::$app->request->post('address_id');
@@ -81,4 +113,29 @@ class AddressController extends AppserverTokenController
         Yii::$service->customer->address->remove($address_id, $customer_id);
     }
     
+    
+    public function coll()
+    {
+        $identity = Yii::$app->user->identity;
+        $customer_id = $identity['id'];
+        $filter = [
+                'numPerPage'    => 100,
+                'pageNum'        => 1,
+                'orderBy'    => ['updated_at' => SORT_DESC],
+                'where'            => [
+                    ['customer_id' => $customer_id],
+                ],
+            'asArray' => true,
+          ];
+        $coll = Yii::$service->customer->address->coll($filter);
+        $arr = [];
+        if (isset($coll['coll']) && !empty($coll['coll'])) {
+            foreach($coll['coll'] as $one){
+                $one['stateName'] = Yii::$service->helper->country->getStateByContryCode($one['country'],$one['state']);
+                $one['countryName'] = Yii::$service->helper->country->getCountryNameByKey($one['country']); 
+                $arr[] = $one;
+            }
+        }
+        return $arr;
+    }
 }
