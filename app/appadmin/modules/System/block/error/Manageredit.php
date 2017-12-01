13eaf275274a6fa9c882eb2273d28be0d2edd912
Terry <2358269014@qq.com>
2017-12-01 23:47:15 +0800

    代码注释调整

--- app/appadmin/modules/System/block/error/Manageredit.php
+++ app/appadmin/modules/System/block/error/Manageredit.php
@@ -22,14 +22,7 @@ use Yii;
  */
 class Manageredit //extends AppadminbaseBlockEdit implements AppadminbaseBlockEditInterface
 {
-    //public $_saveUrl;
-
-    //public function init()
-    //{
-    //    //$this->_saveUrl = CUrl::getUrl('cms/article/managereditsave');
-    //    parent::init();
-    //}
-
+    
     // 传递给前端的数据 显示编辑form
     public function getLastData()
     {
@@ -37,90 +30,8 @@ class Manageredit //extends AppadminbaseBlockEdit implements AppadminbaseBlockEd
         $primaryVal = Yii::$app->request->get($primaryKey);
         $errorHander = Yii::$service->helper->errorHandler->getByPrimaryKey($primaryVal);
         return $errorHander->attributes;
-        //return [
-        //    'editBar'     => $this->getEditBar(),
-        //    'textareas'   => $this->_textareas,
-        //    'lang_attr'   => $this->_lang_attr,
-        //    'saveUrl'     => $this->_saveUrl,
-        //];
-    }
-    /**
-    public function setService()
-    {
-        $this->_service = Yii::$service->helper->errorHandler;
-    }
-
-    public function getEditArr()
-    {
-        return [
-            [
-                'label'=>'标题',
-                'name'=>'category',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-
-            [
-                'label'=>'状态码',
-                'name'=>'code',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-            
-            [
-                'label'=>'Message',
-                'name'=>'message',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-            
-            [
-                'label'=>'File',
-                'name'=>'file',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-            
-            [
-                'label'=>'line',
-                'name'=>'line',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-            
-            [
-                'label'=>'Ip',
-                'name'=>'ip',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-            
-            [
-                'label'=>'Name',
-                'name'=>'name',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-
-            [
-                'label'=>'Url',
-                'name'=>'url',
-                'display'=>[
-                    'type' => 'inputString',
-                ],
-            ],
-
-            
-        ];
+        
     }
     
-    */
 
 }
