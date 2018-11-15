40705e405e643a1498be41ce035b2247158c8f9d
Terry <2358269014@qq.com>
2018-11-16 00:47:09 +0800

    admin system error translate

--- app/appadmin/theme/base/default/system/error/manageredit.php
+++ app/appadmin/theme/base/default/system/error/manageredit.php
@@ -20,86 +20,84 @@ use fecadmin\models\AdminRole;
 .dialog .pageContent .pageFormContent{background:none;}
 </style>
 
-
 <div layouth="56" class="pageFormContent" style="height: 485px; overflow: auto;">
-
+    <div>
+        <input value="5a211ce8bfb7ae649c0e7976" size="30" name="editFormData[_id]" class="textInput " type="hidden">							<p class="edit_p">
+            <label><?= Yii::$service->page->translate->__('Category') ?>：</label>
+            <input value="<?= $category  ?>" size="30" name="editFormData[category]" class="textInput" type="text">
+        </p>							<p class="edit_p">
+            <label><?= Yii::$service->page->translate->__('Status Code') ?>：</label>
+            <input value="<?= $code  ?>" size="30" name="editFormData[code]" class="textInput" type="text">
+        </p>							
+        <p class="edit_p">
+            <label><?= Yii::$service->page->translate->__('Line') ?>：</label>
+            <input value="<?= $line  ?>" size="30" name="editFormData[line]" class="textInput" type="text">
+        </p>							<p class="edit_p">
+            <label><?= Yii::$service->page->translate->__('Ip') ?>：</label>
+            <input value="<?= $ip  ?>" size="30" name="editFormData[ip]" class="textInput" type="text">
+        </p>							<p class="edit_p">
+            <label><?= Yii::$service->page->translate->__('Error Name') ?>：</label>
+            <input value="<?= $name  ?>" size="30" name="editFormData[name]" class="textInput" type="text">
+        </p>
+                            
+        <div style="clear:both"></div>
+    </div>
+    <div>
+        <label><?= Yii::$service->page->translate->__('Url') ?>：</label>
+         <input value="<?= $url  ?>"  style="width:80%"   />
+    </div>
     
-        <div>
-            <input value="5a211ce8bfb7ae649c0e7976" size="30" name="editFormData[_id]" class="textInput " type="hidden">							<p class="edit_p">
-                <label>分类：</label>
-                <input value="<?= $category  ?>" size="30" name="editFormData[category]" class="textInput" type="text">
-            </p>							<p class="edit_p">
-                <label>状态码：</label>
-                <input value="<?= $code  ?>" size="30" name="editFormData[code]" class="textInput" type="text">
-            </p>							
-            <p class="edit_p">
-                <label>line：</label>
-                <input value="<?= $line  ?>" size="30" name="editFormData[line]" class="textInput" type="text">
-            </p>							<p class="edit_p">
-                <label>Ip：</label>
-                <input value="<?= $ip  ?>" size="30" name="editFormData[ip]" class="textInput" type="text">
-            </p>							<p class="edit_p">
-                <label>Name：</label>
-                <input value="<?= $name  ?>" size="30" name="editFormData[name]" class="textInput" type="text">
-            </p>
-            					
-            <div style="clear:both"></div>
-        </div>
-        <div>
-            <label>Url：</label>
-             <input value="<?= $url  ?>"  style="width:80%"   />
-        </div>
+    <div style="clear:both"></div>
+    <br/>
+    <div>
+        <label><?= Yii::$service->page->translate->__('File') ?>File：</label>
+         <input value="<?= $file  ?>"  style="width:80%"   />
+    </div>
+    <div style="clear:both"></div>
+    <br/>
+    <div>
+        <label><?= Yii::$service->page->translate->__('Message') ?>: </label>
+         <input value="<?= $message  ?>"  style="width:80%"   />
+    </div>
+    <div style="clear:both"></div>
+    <div style="padding:10px 0">
+        <label><?= Yii::$service->page->translate->__('Request Info') ?></label>
         
-        <div style="clear:both"></div>
-        <br/>
-        <div>
-            <label>File：</label>
-             <input value="<?= $file  ?>"  style="width:80%"   />
-        </div>
-        <div style="clear:both"></div>
-        <br/>
-        <div>
-            <label>Message：</label>
-             <input value="<?= $message  ?>"  style="width:80%"   />
-        </div>
-        <div style="clear:both"></div>
-        <div style="padding:10px 0">
-            <label>Request_Info</label>
-            
-            <div style="width:80%;float:left;line-height:20px;background:#fff;padding:5px;" >
-                <?php
-                    
+        <div style="width:80%;float:left;line-height:20px;background:#fff;padding:5px;" >
+            <?php
+                if (is_array($request_info)) {
                     foreach ($request_info as $k =>$v){
                         if (is_array($v)) {
                             $v = json_encode($v);
                         } 
-                ?><?= $k; ?> : <?= $v  ?><br/>
-                <?php
+            ?>
+                        <?= $k; ?> : <?= $v  ?><br/>
+            <?php
                     }
-                
-                ?>
-            </div>
+                } else {
+                    echo 'null';
+                }
+            ?>
         </div>
-        <div style="clear:both"></div>
-        <div style="padding:10px 0;margin:10px 0 0;">
-            <label>trace string</label>
-            
-            <div style="width:80%;float:left;line-height:20px;background:#fff;padding:5px;" >
-                <?php
-                    $trace_string = explode('#',$trace_string);
-                    foreach ($trace_string as $k =>$v){
-                        
-                        $v = trim($v);
-                        if(!$v) {
-                            continue;
-                        }
-                ?>
-                    #<?= $v  ?><br/>
-                <?php
+    </div>
+    <div style="clear:both"></div>
+    <div style="padding:10px 0;margin:10px 0 0;">
+        <label><?= Yii::$service->page->translate->__('Trace Info') ?></label>
+        
+        <div style="width:80%;float:left;line-height:20px;background:#fff;padding:5px;" >
+            <?php
+                $trace_string = explode('#',$trace_string);
+                foreach ($trace_string as $k =>$v){
+                    
+                    $v = trim($v);
+                    if(!$v) {
+                        continue;
                     }
-                
-                ?>
-            </div>
+            ?>
+                #<?= $v  ?><br/>
+            <?php
+                }
+            ?>
         </div>
-    
+    </div>
 </div>
