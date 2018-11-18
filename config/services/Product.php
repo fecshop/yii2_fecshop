e50da0f13233085d6400d6ac45b805b3e8cc2f4c
Terry <2358269014@qq.com>
2018-11-18 22:40:37 +0800

    添加注释

--- config/services/Product.php
+++ config/services/Product.php
@@ -14,6 +14,12 @@ return [
          * 因此对总数进行了限制。
          */
         'categoryAggregateMaxCount' => 6000,
+        /**
+          * 分类页面的产品，如果一个spu下面由多个sku同时在这个分类，
+          * 那么，是否只显示一个sku（score最高），而不是全部sku
+          * true： 代表只显示一个sku
+          * false: 代表产品全部显示
+          */
         'productSpuShowOnlyOneSku' => true,
         // 'customAttrGroup' => [], 详细参看@common/config/fecshop_local_services/Product.php 里面的配置
         // 子服务
