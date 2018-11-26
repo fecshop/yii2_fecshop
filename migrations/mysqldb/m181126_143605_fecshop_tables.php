dc3bda3dc01cf281e61a96a27594c9959f81cdc2
Terry <2358269014@qq.com>
2018-11-26 22:52:45 +0800

    db sql update

--- migrations/mysqldb/m181126_143605_fecshop_tables.php
+++ migrations/mysqldb/m181126_143605_fecshop_tables.php
@@ -3,21 +3,23 @@
 use yii\db\Migration;
 
 /**
- * Class m181118_003931_fecshop_tables
+ * Class m181126_143605_fecshop_tables
  */
-class m181118_003931_fecshop_tables extends Migration
+class m181126_143605_fecshop_tables extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
-        $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Product Review Save', 'catalog_product_review_manager', 2, '/catalog/productreview/managereditsave', 1542501354, 1542501354, 1)");
+        $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Error Page', 'dashboard_main', 2, '/fecadmin/error/index', 1543241523, 1543241523, 1)");
         
         $lastInsertId = $this->db->getLastInsertID() ;
         
         $this->execute("INSERT INTO `admin_role_url_key` (`role_id`, `url_key_id`, `created_at`, `updated_at`) VALUES (4, " . $lastInsertId . ", 1541129239, 1541129239)");
         
+        
+
     }
 
     /**
@@ -25,7 +27,7 @@ class m181118_003931_fecshop_tables extends Migration
      */
     public function safeDown()
     {
-        echo "m181118_003931_fecshop_tables cannot be reverted.\n";
+        echo "m181126_143605_fecshop_tables cannot be reverted.\n";
 
         return false;
     }
@@ -39,7 +41,7 @@ class m181118_003931_fecshop_tables extends Migration
 
     public function down()
     {
-        echo "m181118_003931_fecshop_tables cannot be reverted.\n";
+        echo "m181126_143605_fecshop_tables cannot be reverted.\n";
 
         return false;
     }
