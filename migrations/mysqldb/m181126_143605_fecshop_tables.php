d8a34de415643290a97acdce6b82d987ca487a85
Terry <2358269014@qq.com>
2018-11-27 12:01:25 +0800

    sql update

--- migrations/mysqldb/m181126_143605_fecshop_tables.php
+++ migrations/mysqldb/m181126_143605_fecshop_tables.php
@@ -12,14 +12,28 @@ class m181126_143605_fecshop_tables extends Migration
      */
     public function safeUp()
     {
+        // 1
         $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Error Page', 'dashboard_main', 2, '/fecadmin/error/index', 1543241523, 1543241523, 1)");
+        
+        $lastInsertId = $this->db->getLastInsertID() ;
+        
+        $this->execute("INSERT INTO `admin_role_url_key` (`role_id`, `url_key_id`, `created_at`, `updated_at`) VALUES (4, " . $lastInsertId . ", 1541129239, 1541129239)");
+
+        // 2
+        $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Upload Category Image', 'catalog_category_manager', 6, '/catalog/category/imageupload', 1543250331, 1543250420, 1)");
+        
+        $lastInsertId = $this->db->getLastInsertID() ;
+        
+        $this->execute("INSERT INTO `admin_role_url_key` (`role_id`, `url_key_id`, `created_at`, `updated_at`) VALUES (4, " . $lastInsertId . ", 1541129239, 1541129239)");
+        // 3
+        $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Editor Upload Image', 'dashboard_main', 4, '/cms/xeditor/imageupload', 1543252658, 1543252658, 1)");
         
         $lastInsertId = $this->db->getLastInsertID() ;
         
         $this->execute("INSERT INTO `admin_role_url_key` (`role_id`, `url_key_id`, `created_at`, `updated_at`) VALUES (4, " . $lastInsertId . ", 1541129239, 1541129239)");
         
         
-
+        
     }
 
     /**
