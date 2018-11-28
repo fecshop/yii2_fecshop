c4a8519d2250c9b582d49683cc9cb863bd9f84d7
Terry <2358269014@qq.com>
2018-11-28 22:56:30 +0800

    apphtml customer address csrf

--- app/apphtml5/theme/base/html5/customer/address/index.php
+++ app/apphtml5/theme/base/html5/customer/address/index.php
@@ -6,6 +6,8 @@
  * @copyright Copyright (c) 2016 FecShop Software LLC
  * @license http://www.fecshop.com/license/
  */
+
+use fec\helpers\CRequest;
 ?>
 <div class="account-ds">
 	<div class="bar bar-nav account-top-m">
@@ -66,15 +68,13 @@
 			</div>
 		</div>
 		<script>
-		 function deleteAddress(address_id){
-			var r=confirm('do you readly want delete this address?'); 
-			if (r==true){ 
-				url = "<?= Yii::$service->url->getUrl('customer/address') ?>?method=remove&address_id="+address_id;
-				
-				window.location.href=url;
-			}
-		 }
-
+            function deleteAddress(address_id){
+                var r=confirm("<?= Yii::$service->page->translate->__('do you readly want delete this address?') ?>");
+                if (r==true){
+                    url = "<?= Yii::$service->url->getUrl('customer/address') ?>";
+                    doPost(url, {"method": "remove", "address_id": address_id, "<?= CRequest::getCsrfName() ?>": "<?= CRequest::getCsrfValue() ?>" });
+                }
+            }
 		</script>
 	</div>
 	
