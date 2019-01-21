2c5fdb4300b2dc7a33151f59120e940e45d1802d
Terry <2358269014@qq.com>
2019-01-21 12:11:38 +0800

    vue appserver redis session

--- services/session/SessionRedis.php
+++ services/session/SessionRedis.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * FecShop file.
  *
@@ -7,14 +6,10 @@
  * @copyright Copyright (c) 2016 FecShop Software LLC
  * @license http://www.fecshop.com/license/
  */
-
 namespace fecshop\services\session;
-
 use Yii;
 use fecshop\services\Service;
-
 //use fecshop\models\mysqldb\SessionStorage;
-
 /**
  * redis session services
  * @author Terry Zhao <2358269014@qq.com>
@@ -22,65 +17,61 @@ use fecshop\services\Service;
  */
 class SessionRedis extends Service implements SessionInterface
 {
-    protected $storageSession;
-    
+    protected $valSeparator = '||####||';
+
     public function init()
     {
         parent::init();
-        // SessionRedis 是单例模式，因此init只能执行一次，下面的也是只会执行一次。
-        $this->storageSession = new \yii\redis\Session;
     }
-    
+
     public function set($key, $val, $timeout)
     {
-        $uuid = Yii::$service->session->getUUID();
-        $r_id = $this->getUuidKey($uuid, $key);
-
-        return $this->storageSession->set($r_id, $val, $timeout);
+        $key = $this->getSessionKey($key);
+        $val = $val . $this->valSeparator . time();
+        return (bool) Yii::$app->redis->executeCommand('SET', [$key, $val, 'EX', $timeout]);
     }
-
-    public function get($key, $reflush)
+    public function get($originKey, $reflush)
     {
-        $uuid = Yii::$service->session->getUUID();
-        $r_id = $this->getUuidKey($uuid, $key);
-
-        return $this->storageSession->get($r_id);
+        $key = $this->getSessionKey($originKey);
+        $data = Yii::$app->redis->executeCommand('GET', [$key]);
+        $arr = explode($this->valSeparator, $data);
+        if (count($arr) < 2) {
+            return '';
+        }
+        $val = $arr[0];
+        $timeout = $arr[1];
+        if (Yii::$service->session->isUpdateTimeOut($timeout) && $val) {
+            $this->set($originKey, $val, $timeout);
+        }
+        return $val === false || $val === null ? '' : $val;
     }
-
     public function remove($key)
     {
-        $uuid = Yii::$service->session->getUUID();
-        $r_id = $this->getUuidKey($uuid, $key);
-
-        return $this->storageSession->remove($r_id);
+        $key = $this->getSessionKey($key);
+        Yii::$app->redis->executeCommand('DEL', [$key]);
+        // @see https://github.com/yiisoft/yii2-redis/issues/82
+        return true;
     }
-    
-    public function getUuidKey($uuid, $key)
+
+    public function getSessionKey($key)
     {
+        $uuid = Yii::$service->session->getUUID();
         return $uuid.'###^^###'.$key;
     }
-
     /**
      * 销毁所有
      */
     public function destroy()
     {
-        return $this->storageSession->destroy();
-    }
 
+    }
     public function setFlash($key, $val, $timeout)
     {
-        $uuid = Yii::$service->session->getUUID();
-        $r_id = $this->getUuidKey($uuid, $key);
 
-        return $this->storageSession->setFlash($r_id, $val, $timeout);
     }
-    
+
     public function getFlash($key)
     {
-        $uuid = Yii::$service->session->getUUID();
-        $r_id = $this->getUuidKey($uuid, $key);
 
-        return $this->storageSession->getFlash($r_id);
     }
 }
