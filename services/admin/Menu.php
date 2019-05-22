61fe92fb1d7176eecf3a4f18883781e7e646484c
Terry <2358269014@qq.com>
2019-05-22 10:26:47 +0800

    后台菜单，可以通过配置active将某些菜单在后台不显示

--- services/admin/Menu.php
+++ services/admin/Menu.php
@@ -48,6 +48,10 @@ class Menu extends Service
     public function getLeftMenuTreeHtml($treeArr='', $i=1){
         $str = '';
         foreach($treeArr as $node){
+            // 二次开发的过程中，如果fecshop后台的某些菜单想不显示，那么可以在配置中将active设置成false
+            if (isset($node['active']) && $node['active'] === false) {
+                continue;
+            }
             $name = Yii::$service->page->translate->__($node["label"]);
             $url_key = $node["url_key"];
             $roleUrlKeys = $this->getRoleUrlKey();
