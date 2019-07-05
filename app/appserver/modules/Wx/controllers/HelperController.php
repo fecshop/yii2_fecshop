7ae460bd1bd7b12e795726cee4fac0115a2f1453
Terry <2358269014@qq.com>
2019-07-05 17:42:30 +0800

    add wx helper controller

---
+++ app/appserver/modules/Wx/controllers/HelperController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\app\appserver\modules\Wx\controllers;
+
+use fecshop\app\appserver\modules\AppserverController;
+use Yii;
+ 
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0 
+ */
+class HelperController extends AppserverController
+{
+    //   生成分享二维码
+    /*
+    public function actionQrcode()
+    {
+        POST https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=ACCESS_TOKEN
+        
+        access_token
+        
+    }
+    */
+    
+    
+}
