20e79eb1617a6e05f89cb29104e9b933a5539fd8
Terry <2358269014@qq.com>
2019-07-16 10:52:02 +0800

    category product models

---
+++ models/mysqldb/CategoryProduct.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\models\mysqldb;
+
+use yii\db\ActiveRecord;
+
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class CategoryProduct extends ActiveRecord
+{
+    public static function tableName()
+    {
+        return '{{%category_product}}';
+    }
+}
