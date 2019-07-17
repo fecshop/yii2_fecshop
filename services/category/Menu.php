731a9cb61d2e22cbf2f330d44760e809bcf2aacf
Terry <2358269014@qq.com>
2019-07-17 22:08:25 +0800

    商城菜单部分

--- services/category/Menu.php
+++ services/category/Menu.php
@@ -22,15 +22,15 @@ class Menu extends Service
 {
     public $rootCategoryId = '0';
 
-    protected $_categoryModelName = '\fecshop\models\mongodb\Category';
+    //protected $_categoryModelName = '\fecshop\models\mongodb\Category';
 
-    protected $_categoryModel;
+    //protected $_categoryModel;
     
-    public function init()
-    {
-        parent::init();
-        list($this->_categoryModelName, $this->_categoryModel) = Yii::mapGet($this->_categoryModelName);
-    }
+    //public function init()
+    //{
+        //parent::init();
+        //list($this->_categoryModelName, $this->_categoryModel) = Yii::mapGet($this->_categoryModelName);
+    //}
 
     /**
      * @param $parentId | int
@@ -38,27 +38,39 @@ class Menu extends Service
      */
     protected function actionGetCategoryMenuArr($parentId = '')
     {
-        $model = $this->_categoryModel;
+        //$model = $this->_categoryModel;
         $arr = [];
         if (!$parentId) {
             $parentId = $this->rootCategoryId;
         }
-        $data = $this->_categoryModel->find()->asArray()->select([
-            '_id', 'parent_id', 'name', 'url_key', 'menu_custom',
-        ])->where([
-            'parent_id' => $parentId,
-            'status'    => $model::STATUS_ENABLE,
-            'menu_show' => $model::MENU_SHOW,
-        ])->all();
+        
+        $categoryPrimaryKey = Yii::$service->category->getPrimaryKey();
+        $filter = [
+            'select' => [$categoryPrimaryKey, 'parent_id', 'name', 'url_key', 'menu_custom',],
+            'where' => [
+                ['parent_id' => $parentId,],
+                ['status'    => Yii::$service->category->getCategoryEnableStatus()],
+                ['menu_show' => Yii::$service->category->getCategoryMenuShowStatus()],
+            ],
+        ];
+        $collData = Yii::$service->category->coll($filter);
+        $data = $collData['coll'];
+        //$data = $this->_categoryModel->find()->asArray()->select([
+        //    '_id', 'parent_id', 'name', 'url_key', 'menu_custom',
+        //])->where([
+        //    'parent_id' => $parentId,
+        //    'status'    => $model::STATUS_ENABLE,
+        //    'menu_show' => $model::MENU_SHOW,
+        //])->all();
         if (is_array($data) && !empty($data)) {
             foreach ($data as $category) {
                 $categoryOne = [
-                    '_id'           => (string) $category['_id'],
+                    '_id'           => (string) $category[$categoryPrimaryKey],
                     'name'          => Yii::$service->store->getStoreAttrVal($category['name'], 'name'),
                     'menu_custom'   => Yii::$service->store->getStoreAttrVal($category['menu_custom'], 'menu_custom'),
                     'url'           => Yii::$service->url->getUrl($category['url_key']),
                 ];
-                $childMenu = $this->getCategoryMenuArr((string) $category['_id']);
+                $childMenu = $this->getCategoryMenuArr((string) $category[$categoryPrimaryKey]);
                 if ($childMenu) {
                     $categoryOne['childMenu'] = $childMenu;
                 }
@@ -77,13 +89,6 @@ class Menu extends Service
      */
     protected function hasChild($categoryId)
     {
-        $one = $this->_categoryModel->find()->asArray()->where([
-            'parent_id' => $categoryId,
-        ])->one();
-        if ($one['_id']) {
-            return true;
-        }
-
-        return false;
+        return Yii::$service->category->hasChildCategory($categoryId);
     }
 }
