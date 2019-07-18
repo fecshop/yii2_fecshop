c1c6b419de6b098e8bee44585879aecff51efbe1
Terry <2358269014@qq.com>
2019-07-18 18:36:29 +0800

    product favorite mysql支持

---
+++ models/mysqldb/product/Favorite.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\models\mysqldb\product;
+
+use yii\db\ActiveRecord;
+
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class Favorite extends ActiveRecord
+{
+    
+    public static function tableName()
+    {
+        return '{{%favorite}}';
+    }
+
+}
--- services/product/Favorite.php
+++ services/product/Favorite.php
@@ -21,129 +21,90 @@ use Yii;
  */
 class Favorite extends Service
 {
-    protected $_favoriteModelName = '\fecshop\models\mongodb\product\Favorite';
+    /**
+     * $storagePrex , $storage , $storagePath 为找到当前的storage而设置的配置参数
+     * 可以在配置中更改，更改后，就会通过容器注入的方式修改相应的配置值
+     */
+    public $storage     = 'FavoriteMysqldb';   // FavoriteMysqldb | FavoriteMongodb 当前的storage，如果在config中配置，那么在初始化的时候会被注入修改
 
-    protected $_favoriteModel;
+    /**
+     * 设置storage的path路径，
+     * 如果不设置，则系统使用默认路径
+     * 如果设置了路径，则使用自定义的路径
+     */
+    public $storagePath = '';
+    protected $_favorite;
     
     public function init()
     {
         parent::init();
-        list($this->_favoriteModelName, $this->_favoriteModel) = \Yii::mapGet($this->_favoriteModelName);
+        $currentService = $this->getStorageService($this);
+        $this->_favorite = new $currentService();
     }
     
-    protected function actionGetPrimaryKey()
+    // 动态更改为mongodb model
+    public function changeToMongoStorage()
     {
-        return '_id';
+        $this->storage     = 'FavoriteMongodb';
+        $currentService = $this->getStorageService($this);
+        $this->_favorite = new $currentService();
     }
-
-    protected function actionGetByPrimaryKey($val)
+    
+    // 动态更改为mongodb model
+    public function changeToMysqlStorage()
     {
-        $one = $this->_favoriteModel->findOne($val);
-        if ($one[$this->getPrimaryKey()]) {
-            return $one;
-        } else {
-            return new $this->_favoriteModelName();
-        }
+        $this->storage     = 'FavoriteMysqldb';
+        $currentService = $this->getStorageService($this);
+        $this->_favorite = new $currentService();
     }
-
+    
+    public function getPrimaryKey()
+    {
+        return $this->_favorite->getPrimaryKey();
+    }
+    
+    
+    public function getByPrimaryKey($val)
+    {
+        return $this->_favorite->getByPrimaryKey($val);
+    }
+    
     /**
      * @param $product_id | String ， 产品id
      * @param $user_id | Int ，用户id
      * @return $this->_favoriteModel ，如果用户在该产品收藏，则返回相应model。
      */
-    protected function actionGetByProductIdAndUserId($product_id, $user_id = '')
+    public function getByProductIdAndUserId($product_id, $user_id = '')
     {
-        if (!$user_id) {
-            $identity = Yii::$app->user->identity;
-            $user_id = $identity['id'];
-        }
-        if ($user_id) {
-            $one = $this->_favoriteModel->findOne([
-                'product_id' => $product_id,
-                'user_id'     => $user_id,
-            ]);
-            if ($one[$this->getPrimaryKey()]) {
-                return $one;
-            }
-        }
+        return $this->_favorite->getByProductIdAndUserId($product_id, $user_id);
     }
-
+    
     /**
      * @param $product_id | String ， 产品id
      * @param $user_id | Int ，用户id
      * @return boolean，用户收藏该产品时，执行的操作。
      */
-    protected function actionAdd($product_id, $user_id)
+    public function add($product_id, $user_id)
     {
-        $user_id = (int) $user_id;
-        $productPrimaryKey = Yii::$service->product->getPrimaryKey();
-        $product = Yii::$service->product->getByPrimaryKey($product_id);
-        // 检查产品是否存在，如果不存在，输出报错信息。
-        if (!isset($product[$productPrimaryKey])) {
-            Yii::$service->helper->errors->add('product is not exist!');
-
-            return;
-        }
-        //echo $product_id;exit;
-        $favoritePrimaryKey = Yii::$service->product->favorite->getPrimaryKey();
-        $one = $this->_favoriteModel->findOne([
-            'product_id' => $product_id,
-            'user_id'     => $user_id,
-        ]);
-        if (isset($one[$favoritePrimaryKey])) {
-            $one->updated_at = time();
-            $one->store = Yii::$service->store->currentStore;
-            $one->save();
-
-            return true;
-        }
-        $one = new $this->_favoriteModelName();
-        $one->product_id = $product_id;
-        $one->user_id = $user_id;
-        $one->created_at = time();
-        $one->updated_at = time();
-        $one->store = Yii::$service->store->currentStore;
-        $one->save();
-        // 更新该用户总的收藏产品个数到用户表
-        $this->updateUserFavoriteCount($user_id);
-        $this->updateProductFavoriteCount($product_id);
-
-        return true;
+        return $this->_favorite->add($product_id, $user_id);
     }
-
+    
     /**
      * @param $product_id | String
      * 更新该产品被收藏的总次数。
      */
-    protected function updateProductFavoriteCount($product_id)
+    public function updateProductFavoriteCount($product_id)
     {
-        if ($product_id) {
-            $count = $this->_favoriteModel->find()->where(['product_id'=>$product_id])->count();
-            $product = Yii::$service->product->getByPrimaryKey($product_id);
-            if ($product['_id']) {
-                $product->favorite_count = $count;
-                $product->save();
-            }
-        }
+        return $this->_favorite->updateProductFavoriteCount($product_id);
     }
-
     /**
      * @param $user_id | Int
      * 更新该用户总的收藏产品个数到用户表
      */
-    protected function updateUserFavoriteCount($user_id = '')
+    public function updateUserFavoriteCount($user_id = '')
     {
-        $identity = Yii::$app->user->identity;
-        if (!$user_id) {
-            $user_id = $identity['id'];
-        }
-        if ($user_id) {
-            $count = $this->_favoriteModel->find()->where(['user_id'=>$user_id])->count();
-            $identity->favorite_product_count = $count;
-            $identity->save();
-        }
+        return $this->_favorite->updateUserFavoriteCount($user_id);
     }
-
     /*
      * example filter:
      * [
@@ -158,20 +119,15 @@ class Favorite extends Service
      * 	'asArray' => true,
      * ]
      */
-    protected function actionList($filter)
+    public function list($filter)
     {
-        $query = $this->_favoriteModel->find();
-        $query = Yii::$service->helper->ar->getCollByFilter($query, $filter);
-
-        return [
-            'coll' => $query->all(),
-            'count'=> $query->limit(null)->offset(null)->count(),
-        ];
+        return $this->_favorite->list($filter);
     }
-
-    protected function actionColl($filter)
+    
+    
+    public function coll($filter)
     {
-        return $this->list($filter);
+        return $this->_favorite->coll($filter);
     }
     
     /**
@@ -180,44 +136,15 @@ class Favorite extends Service
      */
     public function removeByProductIdAndUserId($product_id, $user_id)
     {
-        $identity = Yii::$app->user->identity;
-        $user_id = $identity['id'];
-
-        $one = $this->_favoriteModel->findOne([
-            //'_id'        => new \MongoDB\BSON\ObjectId($favorite_id),
-            'user_id'    => $user_id,
-            'product_id' => $product_id,
-        ]);
-        if ($one['_id']) {
-            $one->delete();
-            $this->updateUserFavoriteCount($user_id);
-            $product_id = (string) $one['_id'];
-            $this->updateProductFavoriteCount($product_id);
-
-            return true;
-        }
+        return $this->_favorite->removeByProductIdAndUserId($product_id, $user_id);
     }
-
     /**
      * @param $favorite_id | string
      * 通过id删除favorite
      */
-    protected function actionCurrentUserRemove($favorite_id)
+    public function currentUserRemove($favorite_id)
     {
-        $identity = Yii::$app->user->identity;
-        $user_id = $identity['id'];
-
-        $one = $this->_favoriteModel->findOne([
-            '_id'        => new \MongoDB\BSON\ObjectId($favorite_id),
-            'user_id'    => $user_id,
-        ]);
-        if ($one['_id']) {
-            $one->delete();
-            $this->updateUserFavoriteCount($user_id);
-            $product_id = (string) $one['_id'];
-            $this->updateProductFavoriteCount($product_id);
-
-            return true;
-        }
+        return $this->_favorite->currentUserRemove($favorite_id);
     }
+    
 }
