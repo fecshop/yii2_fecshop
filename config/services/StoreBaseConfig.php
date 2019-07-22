216b9035a530e6d1d2466d0bee2a4e8754c7e3ba
Terry <2358269014@qq.com>
2019-07-22 14:32:35 +0800

    add storeBaseConfig service config

---
+++ config/services/StoreBaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+return [
+    'storeBaseConfig' => [
+        'class' => 'fecshop\services\StoreBaseConfig',
+    ],
+];
