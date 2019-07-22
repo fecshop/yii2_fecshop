2f7ac2af19bd8d7d4b3570391a206d72e6b48205
Terry <2358269014@qq.com>
2019-07-22 14:38:09 +0800

    Store Config

--- components/Store.php
+++ components/Store.php
@@ -19,7 +19,22 @@ use yii\base\Component;
 class Store extends Component implements BootstrapInterface
 {
     public $appName;
+    
+    public $_base_config;
+    
+    protected $_modelName = '\fecshop\models\mysqldb\StoreBaseConfig';
 
+    protected $_model;
+    
+    //
+    public $serviceMongodbName = 'mongodb'; 
+    public $serviceMysqldbName = 'mysqldb'; 
+    //
+    public $enable = 1;
+    public $disable = 2;
+    
+    
+    // 初始化的bootstrap
     public function bootstrap($app)
     {
         if ($this->appName == 'appadmin') {
@@ -28,4 +43,29 @@ class Store extends Component implements BootstrapInterface
             Yii::$service->store->bootstrap($app);
         }
     }
+    // 得到配置值
+    /**
+     * @param $key | string, 配置的主Key
+     * @param $subKey | string, 配置的子key
+     */
+    public function get($key, $subKey = '')
+    {
+        $this->initBaseConfig();
+        if (!$subKey) {
+            return isset($this->_base_config[$key]) ? $this->_base_config[$key] : null;
+        }
+        return isset($this->_base_config[$key][$subKey]) ? $this->_base_config[$key][$subKey] : null;
+    }
+    
+    
+    public function initBaseConfig()
+    {
+        if (!$this->_base_config) {
+            $this->_base_config = Yii::$service->storeBaseConfig->getAllConfig();
+        }
+    }
+    
+    
+    
+    
 }
