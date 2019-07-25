5cea00708bb08e3695e1382fa3d5f1a9145a7476
Terry <2358269014@qq.com>
2019-07-25 20:32:52 +0800

    product attr model and product attr group model

---
+++ models/mysqldb/product/AttrGroup.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\models\mysqldb\product;
+
+use yii\db\ActiveRecord;
+
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class AttrGroup extends ActiveRecord
+{
+    const STATUS_ENABLE  = 1;
+    const STATUS_DISABLE = 2;
+    
+    public static function tableName()
+    {
+        return '{{%product_attribute_group}}';
+    }
+
+}
