70f498170596937726d40b9ec921f436770c6af2
Terry <2358269014@qq.com>
2019-08-05 17:33:49 +0800

    appserver payment

---
+++ app/appadmin/modules/Config/controllers/AppserverpaymentController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\app\appadmin\modules\Config\controllers;
+
+use fecshop\app\appadmin\modules\Config\ConfigController;
+use Yii;
+/**
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class AppserverpaymentController extends ConfigController
+{
+    public $enableCsrfValidation = true;
+    
+    public function actionManager()
+    {
+        $data = $this->getBlock()->getLastData();
+        return $this->render($this->action->id, $data);
+    }
+    
+    
+    public function actionManagersave()
+    {
+        
+        $primaryKey = Yii::$service->customer->getPrimaryKey();
+        
+        $data = $this->getBlock('manager')->save();
+    }
+    
+}
