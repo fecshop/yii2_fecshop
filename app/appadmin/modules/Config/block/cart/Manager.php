f072288de29282369f31ca50f9a99318919273fb
Terry <2358269014@qq.com>
2019-08-08 10:20:35 +0800

    admin cart param config

---
+++ app/appadmin/modules/Config/block/cart/Manager.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\app\appadmin\modules\Config\block\cart;
+
+use fec\helpers\CUrl;
+use fec\helpers\CRequest;
+use fecshop\app\appadmin\interfaces\base\AppadminbaseBlockEditInterface;
+use fecshop\app\appadmin\modules\AppadminbaseBlockEdit;
+use Yii;
+
+/**
+ * block cms\staticblock.
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class Manager extends AppadminbaseBlockEdit implements AppadminbaseBlockEditInterface
+{
+    public $_saveUrl;
+    // 需要配置
+    public $_key = 'cart';
+    public $_type;
+    protected $_attrArr = [
+        'maxCountAddToCart',
+        'addToCartCheckSkuQty',
+    ];
+    
+    public function init()
+    {
+        
+         // 需要配置
+        $this->_saveUrl = CUrl::getUrl('config/cart/managersave');
+        $this->_editFormData = 'editFormData';
+        $this->setService();
+        $this->_param = CRequest::param();
+        $this->_one = $this->_service->getByKey([
+            'key' => $this->_key,
+        ]);
+        if ($this->_one['value']) {
+            $this->_one['value'] = unserialize($this->_one['value']);
+        }
+    }
+    
+    
+    
+    // 传递给前端的数据 显示编辑form
+    public function getLastData()
+    {
+        $id = ''; 
+        if (isset($this->_one['id'])) {
+           $id = $this->_one['id'];
+        } 
+        return [
+            'id'            =>   $id, 
+            'editBar'      => $this->getEditBar(),
+            'textareas'   => $this->_textareas,
+            'lang_attr'   => $this->_lang_attr,
+            'saveUrl'     => $this->_saveUrl,
+        ];
+    }
+    public function setService()
+    {
+        $this->_service = Yii::$service->storeBaseConfig;
+    }
+    public function getEditArr()
+    {
+        $deleteStatus = Yii::$service->customer->getStatusDeleted();
+        $activeStatus = Yii::$service->customer->getStatusActive();
+        
+        return [
+            // 需要配置
+            [
+                'label' => Yii::$service->page->translate->__('addToCartCheckSkuQty'),
+                'name'  => 'addToCartCheckSkuQty',
+                'display' => [
+                    'type' => 'select',
+                    'data' => [
+                        Yii::$app->store->enable => 'Yes',
+                        Yii::$app->store->disable => 'No',
+                    ],
+                ],
+                'remark' => '商品加入购物车，进行产品的库存检查，建议配置NO，无库存商品可以加入购物车，但是不能下单'
+            ],
+            
+            
+            [
+                'label'  => Yii::$service->page->translate->__('maxCountAddToCart'),
+                'name' => 'maxCountAddToCart',
+                'display' => [
+                    'type' => 'inputString',
+                ],
+                'remark' => '单个商品sku加入购物车的最大个数，增加恶意下单占用商品库存的难度',
+            ],
+            
+        ];
+    }
+    
+    public function getArrParam(){
+        $request_param = CRequest::param();
+        $this->_param = $request_param[$this->_editFormData];
+        $param = [];
+        $attrVals = [];
+        foreach($this->_param as $attr => $val) {
+            if (in_array($attr, $this->_attrArr)) {
+                $attrVals[$attr] = $val;
+            } else {
+                $param[$attr] = $val;
+            }
+        }
+        $param['value'] = $attrVals;
+        $param['key'] = $this->_key;
+        
+        return $param;
+    }
+    
+    /**
+     * save article data,  get rewrite url and save to article url key.
+     */
+    public function save()
+    {
+        /*
+         * if attribute is date or date time , db storage format is int ,by frontend pass param is int ,
+         * you must convert string datetime to time , use strtotime function.
+         */
+        // 设置 bdmin_user_id 为 当前的user_id
+        $this->_service->saveConfig($this->getArrParam());
+        $errors = Yii::$service->helper->errors->get();
+        if (!$errors) {
+            echo  json_encode([
+                'statusCode' => '200',
+                'message'    => Yii::$service->page->translate->__('Save Success'),
+            ]);
+            exit;
+        } else {
+            echo  json_encode([
+                'statusCode' => '300',
+                'message'    => $errors,
+            ]);
+            exit;
+        }
+    }
+    
+    
+    
+    public function getVal($name, $column){
+        if (is_object($this->_one) && property_exists($this->_one, $name) && $this->_one[$name]) {
+            
+            return $this->_one[$name];
+        }
+        $content = $this->_one['value'];
+        if (is_array($content) && !empty($content) && isset($content[$name])) {
+            
+            return $content[$name];
+        }
+        
+        return '';
+    }
+}
