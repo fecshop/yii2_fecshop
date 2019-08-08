1d0f65e73655cfe8158099add4cff72b644d67d9
Terry <2358269014@qq.com>
2019-08-09 00:28:54 +0800

    appserver customer module config update

--- app/appserver/config/modules/Customer.php
+++ app/appserver/config/modules/Customer.php
@@ -13,35 +13,35 @@ return [
          * 模块内部的params配置。
          */
         'params'=> [
-            'register' => [
+            //'register' => [
                 // 账号注册成功后，是否自动登录
-                'successAutoLogin' => true,
+             //   'successAutoLogin' => true,
                 /**
                   * 1.注册页面的验证码是否开启
                   * 2.对于appserver 端，是基于api，默认验证码不可用，必须再@common/config/main-local.php中配置redis
                   *    验证码才可用，否则，强行开启会导致报错。
                   */
-                'registerPageCaptcha' => false,
+            //    'registerPageCaptcha' => false,
 
-            ],
-            'login' => [
+            //],
+            //'login' => [
                 /**
                   * 1.登陆页面的验证码是否开启
                   * 2.对于appserver 端，是基于api，默认验证码不可用，必须再@common/config/main-local.php中配置redis
                   *    验证码才可用，否则，强行开启会导致报错。
                   */
-                'loginPageCaptcha' => false,
-
-            ],
-            'forgotPassword' => [
+            //    'loginPageCaptcha' => false,
+            //
+            //],
+            //'forgotPassword' => [
                 /**
                   * 1.忘记密码页面的验证码是否开启
                   * 2.对于appserver 端，是基于api，默认验证码不可用，必须再@common/config/main-local.php中配置redis
                   *    验证码才可用，否则，强行开启会导致报错。
                   */
-                'forgotCaptcha' => false,
-
-            ],
+            //    'forgotCaptcha' => false,
+            //
+            //],
 
             'leftMenu'  => [
                 'Account Information' => '/customer/editaccount',
@@ -52,19 +52,19 @@ return [
 
             ],
 
-            'contacts'    => [
+            //'contacts'    => [
                 /**
                   * 1.联系我们页面的验证码是否开启
                   * 2.对于appserver 端，是基于api，默认验证码不可用，必须再@common/config/main-local.php中配置redis
                   *    验证码才可用，否则，强行开启会导致报错。
                   */
-                'contactsCaptcha' => false,
+            //    'contactsCaptcha' => false,
                 // 设置联系我们邮箱，如果不设置，则从email service配置中读取。
                 //'address' => '',
-            ],
-            'newsletterSubscribe' => [
+            //],
+            //'newsletterSubscribe' => [
 
-            ],
+            //],
         ],
     ]
 ];
