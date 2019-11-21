d93e6a19e88c78a946be01cff986587675cd9741
Terry <2358269014@qq.com>
2019-11-21 11:35:02 +0800

    mysql migrate

--- migrations/mysqldb/m190912_052057_fecshop_tables.php
+++ migrations/mysqldb/m190912_052057_fecshop_tables.php
@@ -27,6 +27,16 @@ class m190912_052057_fecshop_tables extends Migration
         foreach ($arr as $sql) {
             $this->execute($sql);
         }
+        
+        // 1
+        $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Extension Market Administer Test', 'extension_manager', 11, '/system/extensionmarket/administertest', 1574306889, 1574306889, 1)");
+        
+        $lastInsertId = $this->db->getLastInsertID() ;
+        
+        $this->execute("INSERT INTO `admin_role_url_key` (`role_id`, `url_key_id`, `created_at`, `updated_at`) VALUES (4, " . $lastInsertId . ", 1541129239, 1541129239)");
+
+        
+        
     }
 
     /**
