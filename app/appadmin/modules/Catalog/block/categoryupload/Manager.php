4a6183ce6b921ac15519cf9e289980632d256d38
Terry <2358269014@qq.com>
2019-12-20 17:49:45 +0800

    admin category excel mutil upload

---
+++ app/appadmin/modules/Catalog/block/categoryupload/Manager.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+
+namespace fecshop\app\appadmin\modules\Catalog\block\categoryupload;
+
+use fecshop\app\appadmin\interfaces\base\AppadminbaseBlockInterface;
+use fecshop\app\appadmin\modules\AppadminbaseBlock;
+use Yii;
+
+/**
+ * block cms\article.
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+class Manager 
+{
+    
+    public $_fileFullDir;
+    /**
+     * init param function ,execute in construct.
+     */
+    public function init()
+    {
+        /*
+         * service component, data provider
+         */
+        parent::init();
+    }
+
+    public function getLastData()
+    {
+        return [
+        ];
+    }
+    
+    
+    public function uploadCategory()
+    {
+        $fileFullDir = Yii::getAlias('@appadmin/runtime/upload/');
+        if ($this->saveUploadExcelFile($fileFullDir)) {
+            //echo $this->_fileFullDir;
+            
+            $arr = \fec\helpers\CExcel::getExcelContent($this->_fileFullDir);
+            $i = 0;
+            if (is_array($arr) && !empty($arr)) {
+                foreach ($arr as $one) {
+                    $i++;
+                    if ($i > 1) {
+                        $saveStatus = $this->saveCategory($one);
+                        if (!$saveStatus) {
+                            $errorMessage = Yii::$service->helper->errors->get(',');
+                            echo  json_encode([
+                                'statusCode' => '300',
+                                'message'    => $errorMessage,
+                            ]);
+                            exit;
+                        }
+                    }
+                }
+            }
+            echo  json_encode([
+                'statusCode' => '200',
+                'message'    => Yii::$service->page->translate->__('Save Success'),
+            ]);
+            exit;
+        }
+        
+    }
+    
+    public function saveCategory($one)
+    {
+        // 从excel中获取数据。
+        $category_id = $one['A'];
+        $parent_id = $one['B'];
+        $language_code = $one['C'];
+        $category_name = $one['D'];
+        $status = $one['E'];
+        $menu_show = $one['F'];
+        $url_key = $one['G'];
+        $description = $one['H'];
+        $title = $one['I'];
+        $meta_keywords = $one['J'];
+        $meta_description = $one['K'];
+        $image = $one['L'];
+        $thumbnail_image = $one['M'];
+        
+        
+        $nameLang = Yii::$service->fecshoplang->getLangAttrName('name',$language_code);
+        $titleLang = Yii::$service->fecshoplang->getLangAttrName('title',$language_code);
+        $metaKeywordsLang = Yii::$service->fecshoplang->getLangAttrName('meta_keywords',$language_code);
+        $metaDescriptionLang = Yii::$service->fecshoplang->getLangAttrName('meta_description',$language_code);
+        $descriptionLang = Yii::$service->fecshoplang->getLangAttrName('description',$language_code);
+        
+        $categoryPrimaryKey = Yii::$service->category->getPrimaryKey();
+        //$categoryMode = Yii::$service->category->getByPrimarykey($category_id);
+        //$categoryPrimaryKey = Yii::$service->category->getPrimaryKey();
+        //$category_id = '';
+        //if ($categoryMode && isset($categoryMode[$categoryPrimaryKey]) && $categoryMode[$categoryPrimaryKey]) {
+        //    $category_id = $categoryMode[$categoryPrimaryKey];
+        //}
+        
+        $categoryArr[$categoryPrimaryKey] = $category_id;
+        $categoryArr['parent_id'] = $parent_id;
+        $categoryArr['name'][$nameLang] = $category_name;
+        $categoryArr['status'] = (int)$status;
+        $categoryArr['menu_show'] = (int)$menu_show;
+        $categoryArr['url_key'] = $url_key;
+        $categoryArr['description'][$descriptionLang] = $description;
+        $categoryArr['title'][$titleLang] = $title;
+        $categoryArr['meta_keywords'][$metaKeywordsLang] = $meta_keywords;
+        $categoryArr['meta_description'][$metaDescriptionLang] = $meta_description;
+        $categoryArr['image'] = $image;
+        $categoryArr['thumbnail_image'] = $thumbnail_image;
+        
+        return Yii::$service->category->excelSave($categoryArr);
+    }
+    
+    # 1.保存前台上传的文件。
+	public function saveUploadExcelFile($fileFullDir){
+        
+        $name = $_FILES["file"]["name"];
+        $fileFullDir .= 'category_'.time().'_'.rand(1000,9999);
+        if(strstr($name,'.xlsx')){
+            $fileFullDir .='.xlsx';
+        } else if (strstr($name,'.xls')){
+            $fileFullDir .='.xls';
+        }  
+        $this->_fileFullDir  = $fileFullDir;    
+        $result = @move_uploaded_file($_FILES["file"]["tmp_name"],$fileFullDir);
+        
+		return $result;
+	}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ app/appadmin/theme/base/default/catalog/categoryupload/manager.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * FecShop file.
+ *
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+use fec\helpers\CRequest;
+/** 
+ * @author Terry Zhao <2358269014@qq.com>
+ * @since 1.0
+ */
+?>
+<div class="pageContent" >
+	<form method="post" action="<?=  Yii::$service->url->getUrl('catalog/categoryupload/managerupload') ?>" enctype="multipart/form-data" class="pageForm required-validate" onsubmit="return iframeCallback(this);">
+		<div class="pageFormContent" layoutH="56">
+			<?=  CRequest::getCsrfInputHtml();  ?>
+            <fieldset id="fieldset_table_qbe">
+					<legend style="color:#cc0000">分类文件上传</legend>
+                    <div style="padding:10px;">
+                        <input name="file" type="file"  style="float:left"/>
+                        <br><br><br>
+                        <div style="line-height:30px;">
+                            1.此部分功能为通过excel文件，批量上传分类，您可以点击<a style="color:blue" href="<?= Yii::$service->url->getUrl('download/fecshop_category_upload.zip') ?>" target="_blank">这里下载</a>
+                            Excel样式
+                            <br/>
+                            1.1 上面下载是一个例子，里面包含excel文件和图片文件夹，您需要将图片文件夹上传到 @appimage/common/media/catalog/category/
+                            <br/>
+                            1.2. excel文件里面有一行数据，是一个例子
+                            <br/>
+                            1.3 README.txt是一个关于excel文件的说明，请仔细阅读
+                            <br/>
+                            
+                            1.4 文件上传后，系统会通过category_id查找，是否存在存在分类，如果存在，则更新，如果不存在则添加。
+                            <br/>
+                            
+                            1.5 注意，category_id不能为空，否则无法导入
+                        </div>
+                        <div style="line-height:30px;">    
+                            2.按照格式填写Excel产品数据
+                        </div >
+                        <div style="line-height:30px;">    
+                            3.选择文件，提交即可
+                            
+                        </div>
+                        <div style="line-height:30px;">    
+                            
+                            4.更详细的关于分类上传，参看文档: <a  style="color:blue" href="http://www.fecmall.com/doc/fecshop-guide/instructions/cn-2.0/guide-fecmall_category_product_mutil_upload.html">Fecmall分类产品Excel表格批量上传文档</a>
+                        </div>
+                    </div>
+                    
+            </fieldset>
+            
+		</div>
+		
+        <div class="formBar">
+			<ul>
+				<!--<li><a class="buttonActive" href="javascript:;"><span>保存</span></a></li>-->
+				<li>
+                    <div class="buttonActive"><div class="buttonContent"><button onclick="func('accept')"  value="accept" name="accept" type="submit">上传</button></div></div>
+                </li>
+				<li>
+					<div class="button"><div class="buttonContent"><button type="button" class="close">取消</button></div></div>
+				</li>
+			</ul>
+		</div>
+        
+	</form>
+</div>
