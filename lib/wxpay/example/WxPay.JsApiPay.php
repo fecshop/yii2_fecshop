9bf19049216828a05401ddf63ad0d8b0b0892a25
Terry <2358269014@qq.com>
2020-03-10 10:11:58 +0800

    微信库包 去除 $_SERVER[QUERY_STRING]

--- lib/wxpay/example/WxPay.JsApiPay.php
+++ lib/wxpay/example/WxPay.JsApiPay.php
@@ -43,7 +43,7 @@ class JsApiPay
 		//通过code获得openid
 		if (!isset($_GET['code'])){
 			//触发微信返回code码
-			$baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].$_SERVER['QUERY_STRING']);
+			$baseUrl = urlencode('http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; //.$_SERVER['QUERY_STRING']); 
 			$url = $this->__CreateOauthUrlForCode($baseUrl);
 			Header("Location: $url");
 			exit();
