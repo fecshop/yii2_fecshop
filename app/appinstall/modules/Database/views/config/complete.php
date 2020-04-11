2341774eabb08c9166df17c81e1fe862ee21cffa
Terry <2358269014@qq.com>
2020-04-11 18:39:49 +0800

    fecmall 文字说明修正

--- app/appinstall/modules/Database/views/config/complete.php
+++ app/appinstall/modules/Database/views/config/complete.php
@@ -15,11 +15,9 @@
 
 <p style="font-size:16px;line-height:25px;">1.需要设置安全权限（根目录执行，win不需要执行）：<span style="color:#c7254e">chmod 644 common/config/main-local.php</span></p>
 
-<p style="font-size:16px;line-height:25px;">2.删除安装文件 <span style="color:#c7254e">install.php</span>（为了安全，一定要删除掉）(文件路径为：appfront/web/install.php),  </p>
+<p style="font-size:16px;line-height:25px;">2.删除安装文件 <span style="color:#c7254e">install.php</span>（非必须执行，安装完成后访问install.php会被阻止执行，不过删除掉这个文件会处理的更彻底）(文件路径为：appfront/web/install.php),  </p>
 
-<p style="font-size:16px;line-height:25px;">3.后台默认用户名，用户名密码： <span style="color:#c7254e">admin  admin123</span></p>
-
-<p style="font-size:16px;line-height:25px;">4.访问后台（访问nginx配置的后台appadmin域名），更改<span style="color:#c7254e">admin</span>用户密码，然后根据文档进行后台配置  </p>
+<p style="font-size:16px;line-height:25px;">3.访问后台（访问nginx配置的后台appadmin域名),  继续进行安装配置。 </p>
 
 
 <br/>
