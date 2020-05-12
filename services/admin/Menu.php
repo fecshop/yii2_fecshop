759072591fb113020dfe562a21ee78f96a578bfc
Terry <2358269014@qq.com>
2020-05-12 14:01:18 +0800

    后台菜单权限优化：无子菜单显示的上级菜单隐藏

--- services/admin/Menu.php
+++ services/admin/Menu.php
@@ -43,11 +43,51 @@ class Menu extends Service
         
         return Yii::$service->admin->role->getCurrentRoleResources();
     }
+    /**
+     * @param $nodeL | array, 菜单结点
+     * @param $roleUrlKeys | array, 权限urlKey数组
+     *  查看：当前节点的所有的子节点以及子子节点（递归）的urlKey，是否存在于$roleUrlKeys，只要存在一个，就返回true
+     *  此函数的作用为：查看当前阶段是否存在有权限的子菜单，如果没有，则返回false，当前菜单也将隐藏
+     */
+    public function hasChildRoleUrlKey($nodeL, &$roleUrlKeys)
+    {
+        if (!$this->hasChild($nodeL)) {
+            
+            return false;
+        }
+        $treeArr = $nodeL['child'];
+        if (!is_array($treeArr)) {
+            
+            return false;
+        }
+        foreach ($treeArr as $node) {
+            $url_key = $node["url_key"];
+            if ($url_key) {
+                // 如果存在有权限的urlKey
+                if (isset($roleUrlKeys[$url_key]) && $roleUrlKeys[$url_key]) {
+                    
+                    return true;
+                }
+            } else if($this->hasChild($node)) {
+                // 如果子菜单存在有权限的url Key，返回true
+                if ($this->hasChildRoleUrlKey($node, $roleUrlKeys)) {
+                    
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
 
-    # 得到后台显示菜单（左侧）
+    /**
+     * @param $treeArr | array，菜单数组
+     * @param $i | int， 菜单层级
+     * 得到后台显示菜单（左侧）
+     */
     public function getLeftMenuTreeHtml($treeArr='', $i=1){
         $str = '';
-        foreach($treeArr as $node){
+        foreach ($treeArr as $node) {
             // 二次开发的过程中，如果fecshop后台的某些菜单想不显示，那么可以在配置中将active设置成false
             if (isset($node['active']) && $node['active'] === false) {
                 
@@ -56,33 +96,38 @@ class Menu extends Service
             $name = Yii::$service->page->translate->__($node["label"]);
             $url_key = $node["url_key"];
             $roleUrlKeys = $this->getRoleUrlKey();
-            if($url_key && (!isset($roleUrlKeys[$url_key]) || !$roleUrlKeys[$url_key])){
+            if ($url_key) {
+                if (!isset($roleUrlKeys[$url_key]) || !$roleUrlKeys[$url_key]) {
+                    
+                    continue;
+                }
+            } else if (!$this->hasChildRoleUrlKey($node, $roleUrlKeys)) {  // 查看所有的子菜单，是否存在某个urlkey存在于$roleUrlKeys
                 
                 continue;
             }
-            if($i == 1){
+            if ($i == 1) {
                 $str .=	'<div class="accordionHeader">
 							<h2><span>Folder</span>'.$name .'
                                 <span class="first_collapsable"></span>
                             </h2>
 						</div>
 						<div class="accordionContent">';
-                if($this->hasChild($node)){
+                if ($this->hasChild($node)) {
                     $str .='<ul class="tree treeFolder">';
                     $str .= $this->getLeftMenuTreeHtml($node['child'],$i+1);
                     $str .='</ul>';
                 }
                 $str .=	'</div>';
             }else{
-                if($this->hasChild($node)){
+                if ($this->hasChild($node)) {
                     //$str .=		'<li><a href="'.CUrl::getUrl($url_key).'" target="navTab" rel="page1">'.$name.'</a>';
                     $str .=		'<li><a href="javascript:void(0)" >'.$name.'</a>';
                     $str .=			'<ul>';
                     $str .= $this->getLeftMenuTreeHtml($node['child'],$i+1);
                     $str .=			'</ul>';
                     $str .=		'</li>';
-                }else{
-                    $str .='<li><a href="'.CUrl::getUrl($url_key).'" target="navTab" rel="page1">'.$name.'</a></li>';
+                } else {
+                    $str .= '<li><a href="'.CUrl::getUrl($url_key).'" target="navTab" rel="page1">'.$name.'</a></li>';
                 }
             }
         }
@@ -91,7 +136,7 @@ class Menu extends Service
     }
 
     public function hasChild($node){
-        if(isset($node['child']) && !empty($node['child'])){
+        if (isset($node['child']) && !empty($node['child'])) {
             
             return true;
         }
