080bc5a3420df22e8c82c3740d5aaab6646977d6
Terry <2358269014@qq.com>
2020-09-24 14:26:56 +0800

    add admin url key resource

---
+++ migrations/mysqldb/m200924_022049_fecshop_tables.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200924_022049_fecshop_tables
+ */
+class m200924_022049_fecshop_tables extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        // 1
+        $this->execute("INSERT INTO `admin_url_key` (`name`, `tag`, `tag_sort_order`, `url_key`, `created_at`, `updated_at`, `can_delete`) VALUES ('Catalog Product SpuCode', 'catalog_product_info_manager', 30, '/catalog/productinfo/spucode', 1600914154, 1600914154, 1)");
+        
+        $lastInsertId = $this->db->getLastInsertID() ;
+        
+        $this->execute("INSERT INTO `admin_role_url_key` (`role_id`, `url_key_id`, `created_at`, `updated_at`) VALUES (4, " . $lastInsertId . ", 1585655278, 1585655278)");
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m200924_022049_fecshop_tables cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200924_022049_fecshop_tables cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
