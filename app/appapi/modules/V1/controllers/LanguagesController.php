5d64870282607b61417c10bc9a593e545c56f0d0
Terry <2358269014@qq.com>
2021-02-24 17:43:44 +0800

    appapi：language api

--- app/appapi/modules/V1/controllers/LanguagesController.php
+++ app/appapi/modules/V1/controllers/LanguagesController.php
@@ -32,4 +32,82 @@ class LanguagesController extends AppapiTokenController
         ];
     }
     
+    
+    
+    public function actionSetall()
+    {
+        $languages = Yii::$app->request->post('languages');
+        if (!is_array($languages) || empty($languages)) {
+            echo  json_encode([
+                'statusCode' => '300',
+                'message'    => 'languages can not empty',
+            ]);
+            exit;
+        }
+        $hasDefaultLang = false;
+        $defaultLang = false;
+        foreach ($languages as $k=>$languageOne) {
+            $languages[$k]['search_engine'] = 'mysqlSearch';
+            if ($languageOne['is_default']) {
+                if ($hasDefaultLang) {
+                    // 配置中含有多个default lang，
+                    echo  json_encode([
+                        'statusCode' => '300',
+                        'message'    => 'you can only set one lang is_default = true , other must set false ',
+                    ]);
+                    exit;
+                }
+                $hasDefaultLang = true;
+                $defaultLang = $languageOne['lang_code'];
+            }
+            unset($languages[$k]['is_default']);
+        }
+        if (!$hasDefaultLang) {
+            echo  json_encode([
+                'statusCode' => '300',
+                'message'    => 'you do not set default language',
+            ]);
+            exit;
+        }
+        // set default lang;
+        $this->setDefaultlang($defaultLang);
+        $saveData = [
+            'key' => 'mutil_lang',
+            'value' => $languages,
+        ];
+        
+        Yii::$service->storeBaseConfig->saveConfig($saveData);
+        $errors = Yii::$service->helper->errors->get();
+        if (!$errors) {
+            
+            return [
+                'code'    => 200,
+                'message' => 'set all languages success',
+                'data'    => true,
+            ];
+        } 
+        return [
+            'code'    => 300,
+            'message' => 'set all languages fail',
+            'data'    => [
+                'errors' => $errors,
+            ],
+        ];
+    }
+    /**
+     * @param $defaultLangCode | string,  语言code
+     */
+    public function setDefaultlang($defaultLangCode)
+    {
+        $baseConfig = Yii::$service->storeBaseConfig->getByKey([
+            'key' => 'base_info',
+        ]);
+        $saveData = ['key' => 'base_info'];
+        if ($baseConfig['value']) {
+            $saveData['value'] = unserialize($baseConfig['value']);
+        }
+        $saveData['value']['default_lang'] = $defaultLangCode;
+        //var_dump($saveData['value']['default_lang'] );exit;
+        Yii::$service->storeBaseConfig->saveConfig($saveData);
+    }
 }
