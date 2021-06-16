6faa1c8d59658ddbf9dfceee070690c92850a8ae
Terry <2358269014@qq.com>
2021-06-16 13:42:51 +0800

    event services

--- services/Event.php
+++ services/Event.php
@@ -21,16 +21,16 @@ use Yii;
 class Event extends Service
 {
     public $eventList; //Array
-
+    protected $eventErr;
     /**
      * @param $eventName | String , 时间的名字
-     * @param $data | Array ， 数据数组，将各个数据以数组的方式传递过来。
+     * @param $data | Array or object ， 数据数组或者对象，将各个数据以数组的方式传递过来。
      * 从配置中找到相应的event，然后执行event。
      * event 相当于插代码的感觉。
      */
     public function trigger($eventName, $data)
     {
-        if (!is_array($data)) {
+        if (!is_array($data) && !is_object($data)) {
             //Yii::$service->helper->errors->add('event data must array');
             return;
         }
@@ -44,5 +44,37 @@ class Event extends Service
                 }
             }
         }
+    }
+    /**
+     * @param $errors | string
+     * event 执行，增加错误信息
+     */
+    public function addErr($errors)
+    {
+        $this->eventErr[] = $errors;
+        
+        return true;
+    }
+    /**
+     * @param $split | string, 字符串分隔符
+     */
+    public function getErrStr($split=',')
+    {
+        if (!is_array($this->eventErr) || empty($this->eventErr)) {
+            
+            return '';
+        }
+        return implode($split, $this->eventErr);
+    }
+    /**
+     * @param $split | string, 字符串分隔符
+     */
+    public function getErrArr($split=',')
+    {
+        if (!is_array($this->eventErr) || empty($this->eventErr)) {
+            
+            return '';
+        }
+        return $this->eventErr;
     }
 }
